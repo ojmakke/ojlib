a7ae66179c28d399448c39b7b9e12c97a1a639a8
O jM <ojmakke@yahoo.com>
2017-05-28 01:55:21 -0400

    Added multi-threading to ojmemory

--- ojmemory/main.c
+++ ojmemory/main.c
@@ -1,14 +1,57 @@
+#include <pthread.h>
+#include <stdio.h>
+
+// This has to be after system headers to detect if pthread is included (and used)
 #include "ojmemory.h"
+#include "../ojlogger/ojlogger.h"
+
+struct Params
+{
+  struct HeapBlock* block;
+  int tid;
+};
+
+void* runtest(void* params)
+{
+  struct Params* mParams = params;
+  
+  LOGT("Thread: %d\n", mParams->tid);
+  struct HeapBlock* block = mParams->block;
+
+  int ii;
+  for(ii = 0; ii < 100; ii++)
+    oj_heap_malloc(block, 10);
+  
+  oj_heap_free(block);
+  oj_heap_free(block);
+  oj_heap_free(block);
+  return NULL;
+}
 
 
 int main(int argc, char* argv[])
 {
-  struct HeapBlock* block;
-  block = oj_heap_create();
-  oj_heap_malloc(block, 10);
-  oj_heap_malloc(block, 100000);
-  oj_heap_malloc(block, 1000);
-  oj_heap_free(block);
+
+  int const max = 1000;
+  pthread_t pool[max];
+  int ii;
+
+  struct HeapBlock* commonBlock, *workBlock;
+  commonBlock = oj_heap_create();
+  workBlock = oj_heap_create();
+  struct Params* params;
+
+  for(ii = 0; ii < max; ii++)
+    {
+      printf("Locking Ignore this\n");
+      params = oj_heap_malloc(workBlock, sizeof(struct Params));
+      params->block = commonBlock;
+      params->tid = ii;
+      pthread_create(&pool[ii], NULL, runtest, params);
+    }
+
+  for(ii = 0; ii < max; ii++)
+    pthread_join(pool[ii], NULL);
   
   return 0;
 }
--- ojmemory/ojmemory.c
+++ ojmemory/ojmemory.c
@@ -5,12 +5,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+// If something outside has included this
+#ifdef OJ_POSIX_THREAD
+#include <pthread.h>
+#endif
+
+
 
 struct HeapBlock
 {
   void* allocated; /* The root node will be NULL for empty */
   struct HeapBlock* nextBlock;
-  struct HeapBlock* lastBlock; 
+  struct HeapBlock* lastBlock;
+#ifdef OJ_POSIX_THREAD
+  pthread_mutex_t lock;
+#endif
 };
 
 struct HeapBlock* oj_heap_create()
@@ -25,6 +34,10 @@ struct HeapBlock* oj_heap_create()
   LOGD("Heap Block created at %lu\n", newBlock);
   memset(newBlock, 0, sizeof(struct HeapBlock));
   newBlock->lastBlock = newBlock;
+#ifdef OJ_POSIX_THREAD
+  pthread_mutex_init(&newBlock->lock, NULL);
+#endif
+  
   LOGD("--oj_heap_create()--\n");
   return newBlock;
 }
@@ -38,20 +51,32 @@ void oj_heap_add(struct HeapBlock* heapBlock, void* blockptr)
       return;
     }
 
-  /* Imagine you pass a heap block. We want to add reference at the end */
+  /* Imagine you pass a heap block. We want to add reference at the end 
+     For multithreaded, this is tricky because lastBlock can actually change.
+     So we have to lock mutex
+   */
+#ifdef OJ_POSIX_THREAD
+  LOGD("\t\t\tLocking mutex in add\n");
+  pthread_mutex_lock(&heapBlock->lock);
+  LOGD("\t\t\tLocking SUCCEEDED in add\n");
+#endif
   struct HeapBlock* lastBlock = heapBlock->lastBlock;
   lastBlock->nextBlock = oj_heap_create(); /* Local use*/
   lastBlock->nextBlock->lastBlock = heapBlock->lastBlock;
   
   heapBlock->lastBlock = lastBlock->nextBlock;
   LOGD("New Address of lastBlock is %lu\n", lastBlock);
+#ifdef OJ_POSIX_THREAD
+  LOGD("\t\t\tUnlocking mutex in add\n");
+  pthread_mutex_unlock(&heapBlock->lock);
+#endif
   LOGD("--oj_heap_add--\n");
 }
 
 void oj_heap_free(struct HeapBlock* block)
 {
 
-  LOGD("**oj_hea_free**\n");
+  LOGD("**oj_heap_free**\n");
   
   struct HeapBlock* node;
   if(block == NULL)
@@ -61,6 +86,18 @@ void oj_heap_free(struct HeapBlock* block)
     }
   
   LOGD("About to free block at %lu\n", block);
+  // We have to lock. Imagine freeing and then something adds to us. Bad!
+  // Notice that we do not clear the "main" node, which is supposed
+  // to stay empty, by design. This is so that, after free, if something
+  // adds memory to heap block, no crash will happen.
+  // It is also possible to call this function on same object multiple times
+  // Notice that we only operate on the mutex of the root node
+
+#ifdef OJ_POSIX_THREAD
+  LOGD("\t\t\t\t\t\tLocking mutex in free\n");
+  pthread_mutex_lock(&block->lock);
+  LOGD("\t\t\t\t\t\tLocking SUCCEEDED in free\n");
+#endif
   node = block->nextBlock;
   struct HeapBlock* tmpNode;
   while(node != NULL)
@@ -72,6 +109,15 @@ void oj_heap_free(struct HeapBlock* block)
       node = tmpNode;
       LOGD("Next Node to free is %lu\n", node);
     }
+
+  block->nextBlock = NULL;
+  block->lastBlock = block;
+  block->allocated = NULL;
+
+#ifdef OJ_POSIX_THREAD
+  LOGD("\t\t\t\t\t\tUnlocking mutex in free\n");
+  pthread_mutex_unlock(&block->lock);
+#endif
   LOGD("--oj_heap_free--\n");
 }
 
